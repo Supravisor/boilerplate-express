458cabff6f925678a51f6c084190b36c8136a63c
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-11-16 20:59:16 +1300

    serve static asset

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ app.get("path", function(req, res) {
     res.sendFile(absolutePath = __dirname + "/views/index.html");
 });
 
-app.use(__dirname + /public, express.static());
+app.use(__dirname + "/public", express.static());
 
 
 
