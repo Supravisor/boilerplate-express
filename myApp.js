db3903b788ee07cf51daa0c53c254e3ffe5882c9
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-11-17 10:39:11 +1300

    use the .env file

--- myApp.js
+++ myApp.js
@@ -12,7 +12,11 @@ app.get("/", function(req, res) {
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", function(req, res) {
-    res.json({ "message": "Hello json" });
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+      res.json({ "message": "Hello json".toUpperCase() });
+    } else {
+      res.json({ "message": "Hello json" });
+    }
 });
 
 
