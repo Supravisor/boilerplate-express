a839adb99982cd6326da43d372883ef748479654
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-11-17 13:54:38 +1300

    chain middleware to create a time server

--- myApp.js
+++ myApp.js
@@ -3,6 +3,15 @@ require("dotenv").config();
 let express = require('express');
 let app = express();
 
+app.get("/now", function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+     setTimeout(function() {
+      res.send( {time: req.time} );
+     }, 1000);
+});
+
 console.log("Hello World");
 
 app.get("/", function(req, res) {
@@ -24,13 +33,6 @@ app.get("/json", function(req, res) {
 app.use(function(req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
-});
-
-app.get("/now", function(req, res, next) {
-  req.time = new Date().toString();
-  next();
-}, function(req, res) {
-  res.send( {time: req.time} );
 });
 
 
